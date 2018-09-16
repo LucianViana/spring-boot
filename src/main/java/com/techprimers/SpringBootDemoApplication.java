8c77e5c2e8050c92555af5f125a57dff63ba4f4c
Luciano Viana <luciano.souzaviana@gmail.com>
2018-09-16 03:53:51 +0000

    SpringBootDemoApplication.java edited online with Bitbucket

--- src/main/java/com/techprimers/SpringBootDemoApplication.java
+++ src/main/java/com/techprimers/SpringBootDemoApplication.java
@@ -1,39 +1,19 @@
 package com.techprimers;
 
-import java.util.Collections;
-
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.runner.RunWith;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import com.techprimers.repository.UserJpaRespository;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(
-		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-		classes = SpringBootDemoApplication.class
-)
+@SpringBootApplication 
 @AutoConfigureMockMvc
-@TestPropertySource(locations = "classpath:application-test.properties")
+@TestPropertySource(locations = "classpath:application.properties")
 public class SpringBootDemoApplication {
-    @Autowired
-    MockMvc mockMvc;
-    
-	@Test
-	public void contextLoads() throws Exception {
-		MvcResult mvcResult = mockMvc.perform(
-				MockMvcRequestBuilders.get("/users/all")
-				.accept(MediaType.APPLICATION_JSON)
-		).andReturn();
-		System.out.println(mvcResult.getResponse());
-	}
-	
 
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoApplication.class, args);
+	}
 }
