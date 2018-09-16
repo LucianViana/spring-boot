e520f00d2571ee72abbb9c50f5129cfb8d3d8c85
Luciano Viana <luciano.souzaviana@gmail.com>
2018-09-16 03:45:12 +0000

    SpringBootDemoApplication.java edited online with Bitbucket

--- src/main/java/com/techprimers/SpringBootDemoApplication.java
+++ src/main/java/com/techprimers/SpringBootDemoApplication.java
@@ -1,12 +1,39 @@
 package com.techprimers;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Collections;
 
-@SpringBootApplication
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.techprimers.repository.UserJpaRespository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+		classes = SpringBootDemoApplication.class
+)
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class SpringBootDemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDemoApplication.class, args);
+    @Autowired
+    MockMvc mockMvc;
+    
+	@Test
+	public void contextLoads() throws Exception {
+		MvcResult mvcResult = mockMvc.perform(
+				MockMvcRequestBuilders.get("/users/all")
+				.accept(MediaType.APPLICATION_JSON)
+		).andReturn();
+		System.out.println(mvcResult.getResponse());
 	}
+	
+
 }
