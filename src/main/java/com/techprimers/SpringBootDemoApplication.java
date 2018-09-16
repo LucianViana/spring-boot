631e6b8eaa2ffe161f42d74e66e360067bddc18e
Luciano Viana <luciano.souzaviana@gmail.com>
2018-09-16 03:56:23 +0000

    SpringBootDemoApplication.java edited online with Bitbucket

--- src/main/java/com/techprimers/SpringBootDemoApplication.java
+++ src/main/java/com/techprimers/SpringBootDemoApplication.java
@@ -1,13 +1,10 @@
 package com.techprimers;
 
-import org.junit.runner.RunWith;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootApplication 
 @AutoConfigureMockMvc
 @TestPropertySource(locations = "classpath:application.properties")
